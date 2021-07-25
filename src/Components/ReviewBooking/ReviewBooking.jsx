8d73934bd303139a65aa028c92fe161a528aa581
SumanChakraborty1998 <sumanchakra8145@gmail.com>
2021-07-25 19:32:19 +0530

    Alignment changed

--- src/Components/ReviewBooking/ReviewBooking.jsx
+++ src/Components/ReviewBooking/ReviewBooking.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import styles from "./Review.module.css";
 import BookingDetails from "./BookingDetails";
 import FareDetails from "./FareDetails";
-import Footer from "../FOOTER/Footer";
+import Footer from "../Footer/Footer";
 
 const ReviewBooking = () => {
-  return (
-    <>
-      <div className={styles.container}>
-        <BookingDetails />
-        <FareDetails />
-      </div>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <div className={styles.container}>
+                <BookingDetails />
+                <FareDetails />
+            </div>
+            <Footer />
+        </>
+    );
 };
 
 export default ReviewBooking;
