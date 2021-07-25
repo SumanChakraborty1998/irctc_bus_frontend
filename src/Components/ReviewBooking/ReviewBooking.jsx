f3e0095a69bdae3793d98b71a354da6a983d9b9e
prashanth <vengalaprashanth24@gmail.com>
2021-07-25 07:06:23 +0530

    refactor

--- src/Components/ReviewBooking/ReviewBooking.jsx
+++ src/Components/ReviewBooking/ReviewBooking.jsx
@@ -1,55 +1,14 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import styles from "./Review.module.css";
-import { FaBusAlt } from "react-icons/fa";
-import TextField from "@material-ui/core/TextField";
-import MenuItem from "@material-ui/core/MenuItem";
 import BookingDetails from "./BookingDetails";
+import FareDetails from "./FareDetails";
 
 const ReviewBooking = () => {
   return (
     <>
       <div className={styles.container}>
         <BookingDetails />
-        <div className={styles.right_con}>
-          <div>
-            <div className={styles.heading}>
-              <p>Fare Details</p>
-            </div>
-            <div className={styles.heading}>
-              <div className={styles.fare_info}>
-                <span>Base Fare</span>
-                <label>₹ 2550</label>
-              </div>
-              <div className={styles.fare_info}>
-                <span>Tax</span>
-                <label>₹ 2550</label>
-              </div>
-              <div className={styles.fare_info}>
-                <span>IRCTC Booking Charges</span>
-                <label>₹ 2550</label>
-              </div>
-              <div className={styles.fare_info}>
-                <span>GST(18%)</span>
-                <label>₹ 2550</label>
-              </div>
-              <div className={styles.fare_info}>
-                <span>Total Fare</span>
-                <label>₹ 2550</label>
-              </div>
-              <div className={styles.fare_terms}>
-                <input id="agree" type="checkbox" />
-                <label for="agree">
-                  I agree to the
-                  <a href="javascript:void(0)">Terms &amp; Conditions</a>
-                </label>
-              </div>
-              <div className={styles.fare_button}>
-                <button>Continue to Book</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <FareDetails />
       </div>
     </>
   );
