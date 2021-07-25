2336d4e8360680fb78c13fab2b52549971edac36
SumanChakraborty1998 <sumanchakra8145@gmail.com>
2021-07-25 20:59:23 +0530

    Images added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,19 @@
 import Header from "../Components/HEADER/Header";
 import PackagesSlider from "../Components/PackagesSlider/PackagesSlider";
 import Faq from "../Components/FAQ/Faq";
-import Footer from "../Components/FOOTER/Footer";
+import Footer from "../Components/Footer/Footer";
 
 const Home = () => {
-  return (
-    <div>
-      {/* <Navbar openForm={handleOpen} />
+    return (
+        <div>
+            {/* <Navbar openForm={handleOpen} />
       <LoginModel /> */}
-      <Header />
-      <PackagesSlider />
-      <Faq />
-      <Footer />
-    </div>
-  );
+            <Header />
+            <PackagesSlider />
+            <Faq />
+            <Footer />
+        </div>
+    );
 };
 
 export default Home;
