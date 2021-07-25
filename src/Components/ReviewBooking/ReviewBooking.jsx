7d92db110a8689b0e92ee6ba861f984f624be48e
prashanth <vengalaprashanth24@gmail.com>
2021-07-25 07:18:07 +0530

    retouch

--- src/Components/ReviewBooking/ReviewBooking.jsx
+++ src/Components/ReviewBooking/ReviewBooking.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import styles from "./Review.module.css";
 import BookingDetails from "./BookingDetails";
 import FareDetails from "./FareDetails";
+import Navbar from "../NAVBAR/Navbar";
+import Footer from "../FOOTER/Footer";
 
 const ReviewBooking = () => {
   return (
     <>
+      <Navbar />
       <div className={styles.container}>
         <BookingDetails />
         <FareDetails />
       </div>
+      <Footer />
     </>
   );
 };
